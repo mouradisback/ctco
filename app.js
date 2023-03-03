75416dff690e9548a4710a766638b2b0f7bb9dbf
mouradisback <74367232+mouradisback@users.noreply.github.com>
2023-03-03 15:24:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -116,6 +116,6 @@ app.get('*' ,function(req, res) {
 
 
 
-app.listen(3000, ()=>{
-    console.log("server running on port: 3000")
+app.listen(process.env.PORT, ()=>{
+    console.log("server running on port: " + process.env.PORT)
 })
